4ef5602481727bf33c17f2d4ee8d0ee29a69cdbc
kateyatsk <klaettoe2023@gmail.com>
2025-03-19 10:50:11 +0300

    Initial Commit

---
+++ UITable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITable
+//
+//  Created by Екатерина Яцкевич on 19.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
